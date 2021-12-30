09f2e3713fd55baf93a81c4de384bbff0e9f5899
SangGuite <74284942+SangGuite@users.noreply.github.com>
2021-12-30 19:53:52 +0530

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,11 @@
+class atm:
+
+    def __init__(self,card_no,pin_no):
+        self.card_no = card_no
+        self.pin_no = pin_no
+
+    def CashWithdrawl(self):
+        print("Cash has been withdrawn from your account.")
+
+    def BalanceEnquiry(self):
+        print("Your account balance is low.")
